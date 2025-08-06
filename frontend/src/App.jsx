860d14ada8da68c202bd4684804cb502242f6180
kevin <kevinmpz10@icloud.com>
2025-08-06 15:53:59 -0300

    set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
 
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
