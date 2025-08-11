9a3ac4096849a8d74b52177e54ded05e4c3ae17c
kevin <kevinmpz10@icloud.com>
2025-08-11 18:53:31 -0300

    set up auth firebase

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,28 @@ import { useState } from 'react'
 import '../styles/Login.css'
 import { Link } from 'react-router-dom'
 import Singup from '../components/Singup'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../db/firebase.js'
+
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleRegister = async (e) => {
+
+        e.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("Usuario creado:", userCredential.user);
+            alert("Usuario registrado con éxito!");
+        } catch (error) {
+            console.error("Error en el registro:", error.message);
+            alert(error.message);
+        }
+    };
 
     const [singup, setSingup] = useState(false)
 
@@ -12,11 +32,11 @@ const Login = () => {
         setSingup(!singup)
         console.log('clickeado, singup:', !singup);
     }
-   
+
 
     return (
         <div>
-            {singup ? (<Singup/>) :
+            {singup ? (<Singup correo={email} setEmail={setEmail} setPassword={setPassword} contrasena={password} handleRegister={handleRegister} />) :
                 (<div className='container'>
 
                     < h1 className='title' > Login</h1 >
@@ -24,10 +44,11 @@ const Login = () => {
                     <form className='form-container'>
 
                         <label htmlFor="">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
 
                         <label htmlFor="">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                         <Link to={'/login'} > <button>Iniciar sesión</button></Link>
 
