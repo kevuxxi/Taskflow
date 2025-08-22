e427d74395de3df8f173c343e188d10f50ff90d5
kevin <kevinmpz10@icloud.com>
2025-08-22 17:49:19 -0300

    add task form, add validations

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState, useContext, use } from "react"
 import Header from "../components/Header"
 import TaskList from "../components/TaskList"
 import '../styles/Home.css'
@@ -6,6 +6,9 @@ import { collection, addDoc, query, where, onSnapshot, updateDoc, doc, deleteDoc
 import { db } from "../db/firebase.js"
 import { AuthContext } from "../Context/AuthContext.jsx"
 import Loading from "../components/Loading.jsx"
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import AlertTitle from "@mui/material/AlertTitle"
 
 const Home = () => {
 
@@ -14,10 +17,22 @@ const Home = () => {
     const [tareas, setTareas] = useState([]);
     const [texto, setTexto] = useState('')
     const [loading, setloading] = useState(true);
+    const [error, seterror] = useState(null);
+    const [confirmMessage, setconfirmMessage] = useState(null);
+    const [deleteMessage, setdeleteMessage] = useState(null);
+    const [isAddingTask, setisAddingTask] = useState(false);
 
     const aggTarea = async (texto) => {
 
-        if (texto.trim() === '') return;
+        if (texto.trim() === '') {
+            seterror("❌ El campo no puede estar vacío ❌");
+            setTimeout(() => {
+                seterror(null)
+            }, 2000);
+
+
+            return
+        };
 
         if (!currentUser) {
             console.error("No hay un usuario autenticado.");
@@ -30,13 +45,24 @@ const Home = () => {
             userId: currentUser.uid,
         }
 
+        setisAddingTask(true);
+
         try {
             await addDoc(collection(db, "tasks"), nuevatarea);
-            console.log("Tarea agregada con el ID del usuario.");
+            setconfirmMessage("¡Tarea agregada con éxito!")
+            console.log("✅ Tarea agregada con el ID del usuario. ✅");
         } catch (e) {
             console.error("Error al agregar la tarea:", e);
+        } finally {
+            setisAddingTask(false)
         }
+
+        setTimeout(() => {
+            setconfirmMessage(null)
+        }, 2000);
+
         setTexto('');
+        seterror(null);
     }
 
 
@@ -57,6 +83,11 @@ const Home = () => {
     const eliminarTarea = async (id) => {
         const taskref = doc(db, 'tasks', id);
         await deleteDoc(taskref)
+        setdeleteMessage("❌ Tarea eliminada  ❌")
+        setTimeout(() => {
+            setdeleteMessage(null)
+        }, 2000);
+
     }
 
     useEffect(() => {
@@ -83,10 +114,25 @@ const Home = () => {
     return (
         <div >
             <Header />
+
             <div className="home-container">
                 <input type="text" value={texto} onChange={(e) => setTexto(e.target.value)} placeholder="Escribe una nueva tarea..." />
-                <button onClick={() => (aggTarea(texto))}>Agregar Tarea</button>
+                <button disabled={isAddingTask} onClick={() => (aggTarea(texto))}>Agregar Tarea</button>
             </div>
+            <Stack className="exito" spacing={4}>
+                {error &&
+                    <Alert severity="error" sx={{ fontSize: '1.6rem' }}>
+                        {error}
+                    </Alert>}
+                {confirmMessage &&
+                    <Alert severity="success" sx={{ fontSize: '1.6rem' }} >
+                        {confirmMessage}
+                    </Alert>}
+                {deleteMessage &&
+                    <Alert severity="info" sx={{ fontSize: '1.6rem' }}>
+                        {deleteMessage}
+                    </Alert>}
+            </Stack>
             <TaskList tareas={tareas} tareaCompletada={tareaCompletada} eliminarTarea={eliminarTarea} />
         </div >
     )
